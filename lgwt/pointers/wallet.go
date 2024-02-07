307d2bacf4aae19a5013ed0b75b71a904f895f12
Mykal Machon <mykal@tinybox.dev>
2024-02-07 18:23:20 +0000

    feat(lgwt): finishing pointers & errors
    done the basic intro into pointers, pbr, and errors

--- lgwt/pointers/wallet.go
+++ lgwt/pointers/wallet.go
@@ -1,11 +1,32 @@
 package wallet
 
+import (
+	"errors"
+	"fmt"
+)
+
+// https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/pointers-and-errors#wrapping-up
+// key take-aways
+// pointers: if you need to mutate state, use a pointer. If you don't want a copy, pass by reference with &
+// errors: handle them gracefully, declare them and standardize them in your packages
+// new types from existing ones: useful for adding functionality to existing types through methods. add domain specific context and interfaces.
+
+// generic errors
+var ErrInsufficientFunds = errors.New("insufficent funds to make withrdrawl")
+
 // doing this just to make it descriptive?
 // seems like it's obscuring the type for no real reason but alright
-
 // edit: seems like this makes it easy to add methods
 // to existing types; that makes sense actually.
 type Bitcoin int
+
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
 
 type Wallet struct {
 	balance Bitcoin
@@ -18,6 +39,15 @@ func (w *Wallet) Deposit(amount Bitcoin) {
 	// test the address: the & symbol gets the memory address of a var
 	// fmt.Printf("address of wallet in Deposit is %p \n", &w.balance)
 	w.balance += amount
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+
+	w.balance -= amount
+	return nil
 }
 
 func (w *Wallet) Balance() Bitcoin {
--- lgwt/pointers/wallet_test.go
+++ lgwt/pointers/wallet_test.go
@@ -5,18 +5,54 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
 
-	wallet.Deposit(Bitcoin(10))
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
 
-	got := wallet.Balance()
-
-	// test the address. the & symbol gets the pointer or mem address of a var
-	// fmt.Printf("address of wallet in test is %p \n", &wallet.balance)
-
-	want := Bitcoin(10)
-
-	if got != want {
-		t.Errorf("%#v got %d, wanted %d", wallet, got, want)
+		if got != want {
+			t.Errorf("%#v got %s want %s", wallet, got, want)
+		}
 	}
+
+	assertNoError := func(t testing.TB, got error) {
+		t.Helper()
+		if got != nil {
+			t.Fatal("got an error but didn't want one")
+		}
+	}
+
+	assetError := func(t testing.TB, got error, want error) {
+		t.Helper()
+		if got == nil {
+			t.Fatal("Expected error but did not recieve one")
+		}
+
+		if got.Error() != want.Error() {
+			t.Errorf("got %q but wanted %q", got, want)
+		}
+	}
+
+	t.Run("Deposit BTC", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw BTC", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(10))
+
+		assertNoError(t, err)
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw BTW with insufficient funds", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(10)}
+		err := wallet.Withdraw(Bitcoin(20))
+
+		assetError(t, err, ErrInsufficientFunds)
+		assertBalance(t, wallet, Bitcoin(10))
+	})
 }
